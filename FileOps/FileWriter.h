962610f74d1c5bdc5f45ed7e70c7f507b63248d4
Matthias Schroeder <matthias.schroeder@desy.de>
2016-01-21 14:35:58 +0100

    Add FileWriter

--- FileOps/FileWriter.h
+++ FileOps/FileWriter.h
@@ -14,36 +14,17 @@ class FileWriter {
 public:
   static void write(TNamed* obj, TFile& file, const TString& objName = "");
   static void write(TCanvas* can, TFile& file, const TString& canName = "", const TString& option = "root pdf");
+  static TString toFileName(const TString& name);
 };
 
-  void HistReader::toFiles(TNamed* obj, TFile* file, const TString& objName) {
-    if( file->IsZombie() ) {
-      std::cerr << "ERROR: unable to write to file '" << file->GetName() << std::endl;
-      exit(1);
-    } else {
-      TString name = obj->GetName();
-      TString title = obj->GetTitle();
-      if( objName != "" ) {
-	obj->SetName(objName);
-	obj->SetTitle(objName);
-      }
-      file->WriteTObject(obj);
-      obj->SetName(name);
-      obj->SetTitle(title);
-    }
-  }
-  
 
-  void HistReader::toFiles(TCanvas* can, TFile* file, const TString& canName, const TString& option) {
-    if( file->IsZombie() ) {
-      std::cerr << "ERROR: unable to write to file '" << file->GetName() << std::endl;
-      exit(1);
-    } else {
-      TString name = can->GetName();
-      if( canName != "" ) name = canName;
-      if( option.Contains("root") ) file->WriteTObject(can,name);
-      if( option.Contains("eps") ) can->SaveAs(name+".eps","eps");
-    }
-  }
+TString FileWriter::toFileName(const TString& name) {
+  TString fileName(name);
+  fileName.ReplaceAll(" ","_");
+  fileName.ReplaceAll("__","_");
+
+  return fileName;
+}
+
 
 #endif
